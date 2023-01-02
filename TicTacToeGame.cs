581ac2ea1769a4ef6c2c525f20a63a78968a6149
4uvak404 <roma02696@gmail.com>
2023-01-02 22:21:30 +0300

    Добавлены поля в классе данных

--- TicTacToeGame.cs
+++ TicTacToeGame.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.BotAPI.AvailableTypes;
 
 namespace TelegramBOt
 {
     internal class TicTacToeGame
     {
         public int Id { get; set; }
+        public User Player1 { get; set; }
+        public User Player2 { get; set; }
+        public int CurrentPlayer { get; set; }
+        public bool FlipWinner { get; set; }
+        public bool Going { get; set; } = true;
+        public Message Message { get; set; }
+        public TicTacToeMap Map;
     }
 }
