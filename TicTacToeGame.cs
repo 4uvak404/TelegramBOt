c560bc1db3eee35d0203a9bb36704237230236d8
4uvak404 <roma02696@gmail.com>
2023-01-11 22:54:09 +0300

    Добавлены методы EndGame и MakeTurn в класс TicTacToeGame

--- TicTacToeGame.cs
+++ TicTacToeGame.cs
@@ -23,6 +23,40 @@ namespace TelegramBOt
         public bool Going { get; set; } = false;
         public int TicTacToeMapId { get; set; }
         public TicTacToeMap Map { get; set; }
+        public void EndGame(TicTacToeGame game, int winner)
+        {
+            string winText = "default value";
+            if (winner == -1) winText = "Ничья, никто не победил";
+            if (winner == 1) winText = $"Победил @{game.Player1Username} ({winner})";
+            if (winner == 2) winText = $"Победил @{game.Player2Username} ({winner})";
+            game.Going = false;
+            game.CurrentPlayer = 0;
+        }
+        public bool MakeTurn(int row, int column, TicTacToeGame game)
+        {
+            if (!game.Map.SetElement(row, column))
+            {
+                return false;
+            }
+            int result = game.Map.CheckWin();
+
+            if (!(result >= -1 && result <= 2))
+            {
+                return false;
+            }
+
+            if (result == 0)
+            {
+                game.CurrentPlayer = FlipPlayer(game.CurrentPlayer);
+                return true;
+            }
+            if ((bool)game.FlipWinner!)
+            {
+                result = FlipPlayer(result);
+            }
+            EndGame(game, result);
+            return true;
+        }
         public static int FlipPlayer(int player)
         {
             if (player == 1) return 2;
